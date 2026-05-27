73a37fe4ca772ae49e922999d448af2ffbb87fb1
Angelica Torres Triana <angtorres26@unisalle.edu.co>
2026-05-26 21:28:11 -0500

    Crear objeto Orden a partir de datos extraídos de Word

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from servicios.lector_word import leer_documento
+from modelos.orden import Orden
 
 datos = leer_documento()
 
-print(datos)
+orden = Orden(
+    numero=datos["numero"],
+    fecha=datos["fecha"],
+    cliente=datos["cliente"],
+    origen=datos["ciudad_origen"],
+    destino=datos["ciudad_destino"],
+    peso=datos["peso_total"],
+    paquetes=datos["cantidad_paquetes"],
+    tiempo_entrega=datos["tiempo_entrega"],
+    observaciones=""
+)
+
+print(orden.cliente)
+print(orden.origen)
+print(orden.destino)
