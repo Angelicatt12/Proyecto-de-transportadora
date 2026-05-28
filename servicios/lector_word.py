27565ddfec5e84b9a226127e5675d9d2f69880d4
Angelica Torres Triana <angtorres26@unisalle.edu.co>
2026-05-27 20:13:24 -0500

    Implementación del ingreso del documento Word

--- servicios/lector_word.py
+++ servicios/lector_word.py
@@ -2,8 +2,13 @@ from docx import Document
 
 def leer_documento():
     
-    documento = Document("documentos/orden_001.docx")
-    
+    nombre_archivo = input(
+        "Ingrese el nombre del archivo Word (con extensión .docx): "
+    )
+    documento = Document(
+        f"documentos/{nombre_archivo}"
+    )
+        
     datos = {}
     
     for parrafo in documento.paragraphs:
